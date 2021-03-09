0f3407baa579378520419de0c16b1d9f429f9e3e
TheRubble <therubble@hotmail.com>
2021-03-09 19:21:15 +0000

    Init

---
+++ readme.md
@@ -0,0 +1,49 @@
+# Readme
+
+## Course
+https://github.com/conduktor/kafka-stack-docker-compose
+
+## Warning(s)
+Against all natural wants, do not use the async extensions if you care about the delivery order of the messages. 
+This is due to the tasks completing on the threadpool and scheduling could be different - **A custom scheduler would have probably been better here?**
+
+```
+Another difference is that tasks returned by ProduceAsync compete on thread pool threads, so ordering is not guaranteed. By comparison, delivery report callbacks passed to Produce are all called on the same thread, so the ordering will exactly match the order they are known to be completed by the client.
+```
+
+## Topic guidelines
+
+- Create twice as many partitions as brokers up until 12 partitions.
+- Set compression - Helps with latency.
+- Min.insync reps at least 2 maximum of 3.
+- Replication factor of at least 3 at most 5.
+- Carefully consider the key it'll impact the log compaction stuff later.
+
+## Producer
+
+- Idempotent
+- Acks = All
+- Consider linger to increase batch performance and the compression.
+- Set retries etc, timeout is per operation it's across them all.
+
+## Docker
+
+Export the host IP :
+```
+$env:DOCKER_HOST_IP = 'host.docker.internal'
+```
+
+Shell into the container to the console:
+```
+docker exec -it kafka0 bash
+```
+
+## Create topic
+```
+kafka-topics --bootstrap-server localhost:9092 --create --topic customer.data.v1 --replication-factor 1 --partitions 3 --config compression.type=snappy
+```
+
+## Consumer the topic in the console
+```
+kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic customer.data.v1 --from-beginning --property print.value=true
+```
---
+++ src/KafkaTest.Producer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Newtonsoft.Json;
+
+namespace KafkaTest.Producer
+{
+    class Program
+    {
+
+        private static string SampleTopic = "customer.data.v1";
+        
+        private  static readonly ProducerConfig config = new ProducerConfig
+        {
+            BootstrapServers = "host.docker.internal:9092",
+            Acks = Acks.All,
+            EnableIdempotence = true,
+            CompressionType = CompressionType.Snappy,
+            BatchSize = 10
+            
+        };
+        
+        static async Task Main(string[] args)
+        {
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    
+                    using var file = new StreamReader("../../../../../FakeData/set1.txt");
+                    
+                    dynamic dynJson = JsonConvert.DeserializeObject(file.ReadToEnd());
+
+                    foreach (var item in dynJson)
+                    {
+                        Console.WriteLine("{0} {1}\n", item.Id, item.FullName);
+                        producer.Produce(SampleTopic, new Message<string, string> {Key = Guid.NewGuid().ToString(), Value = item.FullName});
+                    }
+                    
+                    await Task.Delay(5000);
+                    producer.Flush(TimeSpan.FromSeconds(5));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            
+        }
+    }
+}
